03d4e7ea23a964b208b5004a003fd704873c7edb
João Mendes Lopes Neto <joaomendes_ln@MacBook-Pro-de-Joao.local>
2021-08-30 12:56:49 -0300

    Organização do lexer e parser para todos os símbolos terminais

--- lexer/lexer.x
+++ lexer/lexer.x
@@ -22,41 +22,42 @@ tokens :-
   array                           { \p s -> ArrayType p }
   boolean                         { \p s -> BooleanType p }
   char                            { \p s -> CharType p }
+  string                          { \p s -> StringType p }
   void                            { \p s -> VoidType p }
   "("                             { \p s -> OpenParenthese p}
   ")"                             { \p s -> CloseParenthese p}
   "["                             { \p s -> OpenBracket p}
   "]"                             { \p s -> CloseBracket p}
-  if                              { \p s -> If p }
-  else                            { \p s -> Else p }
   "{"                             { \p s -> OpenBracer p}
   "}"                             { \p s -> CloseBracer p}
+  ":"                             { \p s -> Colon p }
+  ";"                             { \p s -> SemiColon p }
+  ","                             { \p s -> Comma p}
+  if                              { \p s -> If p }
+  else                            { \p s -> Else p }
   for                             { \p s -> For p}
+  in                              { \p s -> In p }
+  while                           { \p s -> While p }
   return                          { \p s -> Return p}
   break                           { \p s -> Break p}
   continue                        { \p s -> Continue p}
-  ":"                             { \p s -> Colon p }
-  ";"                             { \p s -> SemiColon p }
-  while                           { \p s -> While p }
-  main                            { \p s -> Main p }
   import                          { \p s -> Import p}
+  struct                          { \p s -> Struct p }
+  main                            { \p s -> Main p }
   print                           { \p s -> Print p }
+  "<<"                            { \p s -> Insertion p}
   read                            { \p s -> Read p}
-  ","                             { \p s -> Comma p}
   ">>"                            { \p s -> Extraction p}
-  "<<"                            { \p s -> Insersion p}
   "="                             { \p s -> Assign p}
-  in                              { \p s -> In p }
   [\-]* $digit+                   { \p s -> Int p (read s) }
   [\-]* $digit+ \. $digit+        { \p s -> Double p (read s) }
-  \" ($string # \")* \"           { \p s -> String p ( drop 1 ( take ((length s) - 1) s) ) }
-  \' ($graphic # \") \'           { \p s -> Char p ( head ( drop 1 ( take ((length s) - 1) s) ) ) }
   ("true" | "false")              { \p s -> Boolean p s }
+  \' ($graphic # \") \'           { \p s -> Char p ( head ( drop 1 ( take ((length s) - 1) s) ) ) }
+  \" ($string # \")* \"           { \p s -> String p ( drop 1 ( take ((length s) - 1) s) ) }
   [\+ \- \* \/ \%]                { \p s -> SymArithLeft p (head s) }
   ("**")                          { \p s -> SymArithRight p s }
   ("--" | "++")                   { \p s -> SymArithUnary p s }
-  ("rot" | "inv")                       { \p s -> SymMatrix p s }
-  struct                        { \p s -> Struct p }
+  ("rot" | "inv")                 { \p s -> SymMatrix p s }
   (\< | \> | "<=" | ">=")         { \p s -> SymRel p s }
   ("==" | "!=")                   { \p s -> SymComp p s }
   ("and" | "or" | "not" | "xor")  { \p s -> SymBool p s }
@@ -74,15 +75,8 @@ data Token =
   ArrayType AlexPosn              |
   BooleanType AlexPosn            |
   CharType AlexPosn               |
+  StringType AlexPosn             |
   VoidType AlexPosn               |
-  In  AlexPosn                    |
-  For AlexPosn                    |
-  If AlexPosn                     |
-  Else AlexPosn                   |
-  Return AlexPosn                 |
-  Break AlexPosn                  |
-  Continue AlexPosn               |
-  SemiColon AlexPosn              |
   OpenParenthese AlexPosn         |
   CloseParenthese AlexPosn        |
   OpenBracket AlexPosn            |
@@ -90,25 +84,33 @@ data Token =
   OpenBracer AlexPosn             |
   CloseBracer AlexPosn            |
   Colon AlexPosn                  |
-  While AlexPosn                  |
-  Main AlexPosn                   |
-  Import AlexPosn                 |
-  Print AlexPosn                  |
-  Read AlexPosn                   |
+  SemiColon AlexPosn              |
   Comma AlexPosn                  |
+  If AlexPosn                     |
+  Else AlexPosn                   |
+  For AlexPosn                    |
+  In  AlexPosn                    |
+  While AlexPosn                  |
+  Return AlexPosn                 |
+  Break AlexPosn                  |
+  Continue AlexPosn               |
+  Import AlexPosn                 |
+  Struct AlexPosn                 |
+  Main AlexPosn                   |
+  Print AlexPosn                  |
+  Insertion AlexPosn              |
+  Read AlexPosn                   |
   Extraction AlexPosn             |
-  Insersion AlexPosn              |
-  Assign AlexPosn                 | 
+  Assign AlexPosn                 |
   Int AlexPosn Int                |
   Double AlexPosn Double          |
+  Boolean AlexPosn String         |
+  Char AlexPosn Char              |
+  String AlexPosn String          |
   SymArithLeft AlexPosn Char      |
   SymArithRight AlexPosn String   |
-  String AlexPosn String          |
-  Char AlexPosn Char              |
-  Boolean AlexPosn String         |
   SymArithUnary AlexPosn String   |
   SymMatrix AlexPosn String       |
-  Struct AlexPosn                 |
   SymRel AlexPosn String          |
   SymComp AlexPosn String         |
   SymBool AlexPosn String         |
@@ -123,41 +125,42 @@ token_posn (MatrixType p) = p
 token_posn (ArrayType p) = p
 token_posn (BooleanType p) = p
 token_posn (CharType p) = p
+token_posn (StringType p) = p
 token_posn (VoidType p) = p
 token_posn (OpenParenthese p) = p
 token_posn (CloseParenthese p) = p
 token_posn (OpenBracket p) = p
 token_posn (CloseBracket p) = p
-token_posn (If p) = p
-token_posn (Else p) = p
 token_posn (OpenBracer p) = p
 token_posn (CloseBracer p) = p
+token_posn (Colon p) = p
+token_posn (SemiColon p) = p
+token_posn (Comma p) = p
+token_posn (If p) = p
+token_posn (Else p) = p
 token_posn (For p) = p
+token_posn (In p) = p
+token_posn (While p) = p
 token_posn (Return p) = p
 token_posn (Break p) = p
 token_posn (Continue p) = p
-token_posn (Colon p) = p
-token_posn (SemiColon p) = p
-token_posn (While p) = p
-token_posn (Main p) = p
 token_posn (Import p) = p
+token_posn (Struct p) = p
+token_posn (Main p) = p
 token_posn (Print p) = p
+token_posn (Insertion p) = p
 token_posn (Read p) = p
-token_posn (Comma p) = p
 token_posn (Extraction p) = p
-token_posn (Insersion p) = p
 token_posn (Assign p) = p
-token_posn (In p) = p
 token_posn (Int p _) = p
 token_posn (Double p _) = p
+token_posn (Boolean p _) = p
+token_posn (Char p _) = p
+token_posn (String p _) = p
 token_posn (SymArithLeft p _) = p
 token_posn (SymArithRight p _) = p
-token_posn (String p _) = p
-token_posn (Char p _) = p
-token_posn (Boolean p _) = p
 token_posn (SymArithUnary p _) = p
 token_posn (SymMatrix p _) = p
-token_posn (Struct p) = p
 token_posn (SymRel p _) = p
 token_posn (SymComp p _) = p
 token_posn (SymBool p _) = p
--- tokens.hs
+++ tokens.hs
@@ -9,50 +9,64 @@ intTypeToken = tokenPrim show update_pos get_token where
   get_token (IntType position) = Just $ IntType position
   get_token _    = Nothing
 
-boolTypeToken :: Parsec [Token] st Token
-boolTypeToken = tokenPrim show update_pos get_token where
-  get_token (BooleanType position) = Just $ BooleanType position
-  get_token _    = Nothing
-
 doubleTypeToken :: Parsec [Token] st Token
 doubleTypeToken = tokenPrim show update_pos get_token where
   get_token (DoubleType position) = Just $ DoubleType position
   get_token _    = Nothing
 
-importToken :: Parsec [Token] st Token
-importToken = tokenPrim show update_pos get_token where
-  get_token (Import position) = Just $ Import position
+complexTypeToken :: Parsec [Token] st Token
+complexTypeToken = tokenPrim show update_pos get_token where
+  get_token (ComplexType position) = Just $ ComplexType position
   get_token _    = Nothing
 
-symArithToken :: Parsec [Token] st Token
-symArithToken = tokenPrim show update_pos get_token where
-  get_token (SymArith position name) = Just $ SymArith position name
+matrixTypeToken :: Parsec [Token] st Token
+matrixTypeToken = tokenPrim show update_pos get_token where
+  get_token (MatrixType position) = Just $ MatrixType position
   get_token _    = Nothing
 
-symToken :: Parsec [Token] st Token
-symToken = tokenPrim show update_pos get_token where
-   get_token (Sym position name) = Just $ Sym position name
-   get_token _    = Nothing
-
-mainToken :: Parsec [Token] st Token
-mainToken = tokenPrim show update_pos get_token where
-  get_token (Main position) = Just $ Main position
+arrayTypeToken :: Parsec [Token] st Token
+arrayTypeToken = tokenPrim show update_pos get_token where
+  get_token (ArrayType position) = Just $ ArrayType position
   get_token _    = Nothing
 
-commaToken :: Parsec [Token] st Token
-commaToken = tokenPrim show update_pos get_token where
-  get_token (Comma position) = Just $ Comma position
+booleanTypeToken :: Parsec [Token] st Token
+booleanTypeToken = tokenPrim show update_pos get_token where
+  get_token (BooleanType position) = Just $ BooleanType position
   get_token _    = Nothing
 
+charTypeToken :: Parsec [Token] st Token
+charTypeToken = tokenPrim show update_pos get_token where
+  get_token (CharType position) = Just $ CharType position
+  get_token _    = Nothing
 
-openParentheseToken ::Parsec [Token] st Token
+stringTypeToken :: Parsec [Token] st Token
+stringTypeToken = tokenPrim show update_pos get_token where
+  get_token (StringType position) = Just $ StringType position
+  get_token _    = Nothing
+
+voidTypeToken :: Parsec [Token] st Token
+voidTypeToken = tokenPrim show update_pos get_token where
+  get_token (VoidType position) = Just $ VoidType position
+  get_token _    = Nothing
+
+openParentheseToken :: Parsec [Token] st Token
 openParentheseToken = tokenPrim show update_pos get_token where
   get_token (OpenParenthese position) = Just $ OpenParenthese position
   get_token _              = Nothing
 
-closeParentheseToken ::Parsec [Token] st Token
+closeParentheseToken :: Parsec [Token] st Token
 closeParentheseToken = tokenPrim show update_pos get_token where
   get_token (CloseParenthese position) = Just $ CloseParenthese position
+  get_token _               = Nothing
+
+openBracketToken :: Parsec [Token] st Token
+openBracketToken = tokenPrim show update_pos get_token where
+  get_token (OpenBracket position) = Just $ OpenBracket position
+  get_token _              = Nothing
+
+closeBracketToken :: Parsec [Token] st Token
+closeBracketToken = tokenPrim show update_pos get_token where
+  get_token (CloseBracket position) = Just $ CloseBracket position
   get_token _               = Nothing
 
 openBracerToken :: Parsec [Token] st Token
@@ -70,100 +84,79 @@ colonToken = tokenPrim show update_pos get_token where
   get_token (Colon position)  = Just $ Colon position
   get_token _      = Nothing
 
-identifierToken :: Parsec [Token] st Token
-identifierToken = tokenPrim show update_pos get_token where
-  get_token (Identifier position name)  = Just $ Identifier position name
-  get_token _          = Nothing
-
-
-
--- symToken = tokenPrim show update_pos get_token where
---   get_token (Sym position name)  = Just $ Sym position name
---   get_token _          = Nothing
-
-trueToken ::Parsec [Token] st Token
-trueToken = tokenPrim show update_pos get_token where
-  get_token (TrueSym position) = Just $ TrueSym position
-  get_token _        = Nothing
-
-falseToken :: Parsec [Token] st Token
-falseToken = tokenPrim show update_pos get_token where
-  get_token (FalseSym position) = Just $ FalseSym position
-  get_token _        = Nothing
-
-boolOPToken :: Parsec [Token] st Token
-boolOPToken = tokenPrim show update_pos get_token where
-  get_token (BoolOP position name) = Just $ BoolOP position name 
-  get_token _        = Nothing
-
-relOPToken :: Parsec [Token] st Token
-relOPToken = tokenPrim show update_pos get_token where
-  get_token (RelOP position name) = Just $ RelOP position name 
-  get_token _        = Nothing
-
-intToken :: Parsec [Token] st Token
-intToken = tokenPrim show update_pos get_token where
-  get_token (Int position name) = Just $ Int position name
-  get_token _        = Nothing
-
-doubleToken :: Parsec [Token] st Token
-doubleToken = tokenPrim show update_pos get_token where
-  get_token (Double position name) = Just $ Double position name
-  get_token _    = Nothing
-
 semiColonToken :: Parsec [Token] st Token
 semiColonToken = tokenPrim show update_pos get_token where
   get_token (SemiColon position)  = Just $ SemiColon position
-  get_token _          = Nothing
+  get_token _      = Nothing
 
-assignToken :: Parsec [Token] st Token
-assignToken = tokenPrim show update_pos get_token where
-  get_token (Assign position) = Just $ Assign position
-  get_token _          = Nothing
-
-update_pos :: SourcePos -> Token -> [Token] -> SourcePos
-update_pos pos _ (tok:_) = pos -- necessita melhoria
-update_pos pos _ []      = pos
-
+commaToken :: Parsec [Token] st Token
+commaToken = tokenPrim show update_pos get_token where
+  get_token (Comma position) = Just $ Comma position
+  get_token _    = Nothing
 
 ifToken :: Parsec [Token] st Token
 ifToken = tokenPrim show update_pos get_token where
-    get_token (If position) = Just $ If position
-    get_token _             = Nothing
+  get_token (If position) = Just $ If position
+  get_token _             = Nothing
 
 elseToken :: Parsec [Token] st Token
 elseToken = tokenPrim show update_pos get_token where
-    get_token (Else position) = Just $ Else position
-    get_token _             = Nothing
+  get_token (Else position) = Just $ Else position
+  get_token _             = Nothing
 
 forToken :: Parsec [Token] st Token
 forToken = tokenPrim show update_pos get_token where
-    get_token (For position) = Just $ For position
-    get_token _             = Nothing
+  get_token (For position) = Just $ For position
+  get_token _             = Nothing
 
 inToken :: Parsec [Token] st Token
 inToken = tokenPrim show update_pos get_token where
-    get_token (In position) = Just $ In position
-    get_token _             = Nothing
+  get_token (In position) = Just $ In position
+  get_token _             = Nothing
 
 whileToken :: Parsec [Token] st Token
 whileToken = tokenPrim show update_pos get_token where
-    get_token (While position) = Just $ While position
-    get_token _             = Nothing
-
-breakToken :: Parsec [Token] st Token
-breakToken = tokenPrim show update_pos get_token where
-    get_token (Break position) = Just $ Break position
-    get_token _             = Nothing
-
-continueToken :: Parsec [Token] st Token
-continueToken = tokenPrim show update_pos get_token where
-    get_token (Continue position) = Just $ Continue position
-    get_token _             = Nothing
+  get_token (While position) = Just $ While position
+  get_token _             = Nothing
 
 returnToken :: Parsec [Token] st Token
 returnToken = tokenPrim show update_pos get_token where
-    get_token (Return position) = Just $ Return position
+  get_token (Return position) = Just $ Return position
+  get_token _             = Nothing
+
+breakToken :: Parsec [Token] st Token
+breakToken = tokenPrim show update_pos get_token where
+  get_token (Break position) = Just $ Break position
+  get_token _             = Nothing
+
+continueToken :: Parsec [Token] st Token
+continueToken = tokenPrim show update_pos get_token where
+  get_token (Continue position) = Just $ Continue position
+  get_token _             = Nothing
+
+importToken :: Parsec [Token] st Token
+importToken = tokenPrim show update_pos get_token where
+  get_token (Import position) = Just $ Import position
+  get_token _    = Nothing
+
+structToken :: Parsec [Token] st Token
+structToken = tokenPrim show update_pos get_token where
+  get_token (Struct position) = Just $ Struct position
+  get_token _    = Nothing
+
+mainToken :: Parsec [Token] st Token
+mainToken = tokenPrim show update_pos get_token where
+  get_token (Main position) = Just $ Main position
+  get_token _    = Nothing
+
+printToken :: Parsec [Token] st Token
+printToken = tokenPrim show update_pos get_token where
+    get_token (Print position) = Just $ Print position
+    get_token _             = Nothing
+
+insertionToken :: Parsec [Token] st Token
+insertionToken = tokenPrim show update_pos get_token where
+    get_token (Insertion position) = Just $ Insertion position
     get_token _             = Nothing
 
 readToken :: Parsec [Token] st Token
@@ -176,12 +169,77 @@ extractionToken = tokenPrim show update_pos get_token where
     get_token (Extraction position) = Just $ Extraction position
     get_token _             = Nothing
 
-printToken :: Parsec [Token] st Token
-printToken = tokenPrim show update_pos get_token where
-    get_token (Print position) = Just $ Print position
-    get_token _             = Nothing
+assignToken :: Parsec [Token] st Token
+assignToken = tokenPrim show update_pos get_token where
+  get_token (Assign position) = Just $ Assign position
+  get_token _          = Nothing
 
-insersionToken :: Parsec [Token] st Token
-insersionToken = tokenPrim show update_pos get_token where
-    get_token (Insersion position) = Just $ Insersion position
-    get_token _             = Nothing
+intToken :: Parsec [Token] st Token
+intToken = tokenPrim show update_pos get_token where
+  get_token (Int position name) = Just $ Int position name
+  get_token _        = Nothing
+
+doubleToken :: Parsec [Token] st Token
+doubleToken = tokenPrim show update_pos get_token where
+  get_token (Double position name) = Just $ Double position name
+  get_token _    = Nothing
+
+booleanToken :: Parsec [Token] st Token
+booleanToken = tokenPrim show update_pos get_token where
+  get_token (Boolean position name) = Just $ Boolean position name
+  get_token _    = Nothing
+
+charToken :: Parsec [Token] st Token
+charToken = tokenPrim show update_pos get_token where
+  get_token (Char position name) = Just $ Char position name
+  get_token _    = Nothing
+
+stringToken :: Parsec [Token] st Token
+stringToken = tokenPrim show update_pos get_token where
+  get_token (String position name) = Just $ String position name
+  get_token _    = Nothing
+
+symArithLeftToken :: Parsec [Token] st Token
+symArithLeftToken = tokenPrim show update_pos get_token where
+   get_token (SymArithLeft position name) = Just $ SymArithLeft position name
+   get_token _    = Nothing
+
+symArithRightToken :: Parsec [Token] st Token
+symArithRightToken = tokenPrim show update_pos get_token where
+   get_token (SymArithRight position name) = Just $ SymArithRight position name
+   get_token _    = Nothing
+
+symArithUnaryToken :: Parsec [Token] st Token
+symArithUnaryToken = tokenPrim show update_pos get_token where
+   get_token (SymArithUnary position name) = Just $ SymArithUnary position name
+   get_token _    = Nothing
+
+symMatrixToken :: Parsec [Token] st Token
+symMatrixToken = tokenPrim show update_pos get_token where
+   get_token (SymMatrix position name) = Just $ SymMatrix position name
+   get_token _    = Nothing
+
+symRelToken :: Parsec [Token] st Token
+symRelToken = tokenPrim show update_pos get_token where
+   get_token (SymRel position name) = Just $ SymRel position name
+   get_token _    = Nothing
+
+symCompToken :: Parsec [Token] st Token
+symCompToken = tokenPrim show update_pos get_token where
+   get_token (SymComp position name) = Just $ SymComp position name
+   get_token _    = Nothing
+
+symBoolToken :: Parsec [Token] st Token
+symBoolToken = tokenPrim show update_pos get_token where
+   get_token (SymBool position name) = Just $ SymBool position name
+   get_token _    = Nothing
+
+symMatrixArithToken :: Parsec [Token] st Token
+symMatrixArithToken = tokenPrim show update_pos get_token where
+   get_token (SymMatrixArith position name) = Just $ SymMatrixArith position name
+   get_token _    = Nothing
+
+identifierToken :: Parsec [Token] st Token
+identifierToken = tokenPrim show update_pos get_token where
+  get_token (Identifier position name)  = Just $ Identifier position name
+  get_token _          = Nothing
